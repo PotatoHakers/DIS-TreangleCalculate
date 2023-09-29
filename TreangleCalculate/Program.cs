645ecc6d96bbbcb1583fe91f6338be3c3e6f7d69
PotatoHakers <s.herza@mail.ru>
2023-09-29 20:46:18 +0300

    first commit

---
+++ TreangleCalculate/Program.cs
@@ -0,0 +1,30 @@
+﻿using TriangleLogic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите длину стороны A: ");
+        double sideA = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите длину стороны B: ");
+        double sideB = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите длину стороны C: ");
+        double sideC = double.Parse(Console.ReadLine());
+
+        Triangle triangle = new Triangle(sideA, sideB, sideC);
+        double area = triangle.CalculateArea();
+
+        if (sideA > 0 && sideB > 0 && sideC > 0)
+        {
+            Console.WriteLine($"Площадь треугольника: {area}");
+            //triangle.Finish();
+            return;
+        }
+        else Console.WriteLine("Все стороны треугольника должны быть положительными");
+
+        triangle.Finish();
+        return;
+    }
+}
---
+++ TreangleLogic/TriangleLogic.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TriangleLogic
+{
+    public class Triangle
+    {
+        private double sideA;
+        private double sideB;
+        private double sideC;
+
+        JsonWriter writer;
+
+        public Triangle(double a, double b, double c)
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+
+            if (a > 0 && b > 0 && c > 0)
+            {
+                sideA = a;
+                sideB = b;
+                sideC = c;
+            }
+            
+        }
+        public double CalculateArea()
+        {
+            
+            
+            
+            double s = (sideA + sideB + sideC) / 2;
+            double result = Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+            writer.WriteStartObject();
+            writer.WritePropertyName("Ввод стороны А");
+            writer.WriteValue(sideA);
+            writer.WritePropertyName("Ввод стороны B");
+            writer.WriteValue(sideB);
+            writer.WritePropertyName("Ввод стороны C");
+            writer.WriteValue(sideC);
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+
+
+    }
+}
